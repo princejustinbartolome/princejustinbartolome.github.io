2e36e22d7a4b58dc6e928b5ad82c280f52774511
ph2150074 <ph2150074@transcosmos-ph.local>
2016-07-13 17:59:40 +0800

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,32 +21,36 @@ $(document).ready(function(){
 	var scrollState =true;
 	
 	$('html,body').on('DOMMouseScroll mousewheel', function ( event ) {
-		if (scrollState == true){
+		
 			
 			if( event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) { //alternative options for wheelData: wheelDeltaX & wheelDeltaY
 			
 				//scroll down
 				if (!$('.section.active').is(':last-child')){
+					if (scrollState == true){
 					scrollState = false;
 					$(this).animate({scrollTop: $('.section.active').next('.section').offset().top},500,function(){
 						$('.section.active').next('.section').addClass('active').siblings('.section').removeClass('active');
 						scrollState = true;
 					});
 				}
+				}
 
 			} else {
 				//scroll up
 				if (!$('.section.active').is(':first-child')){
+					if (scrollState == true){
 					scrollState = false;
 					$(this).animate({scrollTop: $('.section.active').prev('.section').offset().top},500,function(){
 						$('.section.active').prev('.section').addClass('active').siblings('.section').removeClass('active');
 						scrollState = true;
 					});
 				}
+				}
 			}
 			//prevent page fom scrolling
 			return false;
-		}
+		
 
 	});
 
