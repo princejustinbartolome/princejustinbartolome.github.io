4ecb62d84f68cbfe18e86633a60efc95c108da32
ph2150074 <ph2150074@transcosmos-ph.local>
2016-07-13 10:51:07 +0800

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
 		$('.sectionBlock01').css({'height' : $(window).height()});
 	});
 	
+	$(".toggleArea p i").hover(function(){
+		$('.navBlock').slideDown();
+		$(this).parents('.toggleArea').animate({top: "-150px"},300);
+
+	});
+	$(".navBlock").mouseleave(function(){
+		$(this).slideUp();
+		$('.toggleArea').animate({top: "0px"},300);
+	});
+	
+	
 });
