1ce5951e9bee84b8331c8a74230214e8b3688556
princejustinbartolome <pjdb.bartolome@gmail.com>
2016-07-15 17:50:28 +0800

    update portfolio

---
+++ me/assets/js/custom.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function(){
+	
+	$('.sectionBlock01').css({'height' : $(window).height()});
+	
+	$(window).resize(function(){
+		$('.sectionBlock01').css({'height' : $(window).height()});
+	});
+	
+	$('.toggleArea p i').hover(function(){
+		$('.navBlock').slideDown();
+		$(this).parents('.toggleArea').animate({top: '-150px'},300);
+	});
+	
+	$('.navBlock').mouseleave(function(){
+		$(this).slideUp();
+		$('.toggleArea').animate({top: '0px'},300);
+	});
+
+	
+
+});
